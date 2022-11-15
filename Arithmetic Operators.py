cd6a61f1779d59bb4fe9dd53f190c6ae391825f7
Khafiz <116889143+Khafiz246810@users.noreply.github.com>
2022-11-16 00:42:59 +0600

    Create Arithmetic Operators.py

---
+++ Arithmetic Operators.py
@@ -0,0 +1,21 @@
+# 1 Input
+a = 21
+b = 10
+c = 0
+c = a + b
+print ("Line 1 - Value of c is ", c)
+c = a - b
+print ("Line 2 - Value of c is ", c )
+c = a * b
+print ("Line 3 - Value of c is ", c)
+c = a / b
+print ("Line 4 - Value of c is ", c )
+c = a % b
+print ("Line 5 - Value of c is ", c)
+
+# Output
+#Line 1 - Value of c is  31
+#Line 2 - Value of c is  11
+#Line 3 - Value of c is  210
+#Line 4 - Value of c is  2.1
+#Line 5 - Value of c is  1
