6636d09eff33a7ccac10cc6f2b096e1fed928640
Khafiz <116889143+Khafiz246810@users.noreply.github.com>
2022-11-16 00:49:40 +0600

    Update Arithmetic Operators.py

--- Arithmetic Operators.py
+++ Arithmetic Operators.py
@@ -1,4 +1,5 @@
-# 1 Input
+# 1 
+Input
 a = 21
 b = 10
 c = 0
@@ -13,9 +14,29 @@ print ("Line 4 - Value of c is ", c )
 c = a % b
 print ("Line 5 - Value of c is ", c)
 
-# Output
-#Line 1 - Value of c is  31
-#Line 2 - Value of c is  11
-#Line 3 - Value of c is  210
-#Line 4 - Value of c is  2.1
-#Line 5 - Value of c is  1
+Output:
+Line 1 - Value of c is  31
+Line 2 - Value of c is  11
+Line 3 - Value of c is  210
+Line 4 - Value of c is  2.1
+Line 5 - Value of c is  1
+
+
+# 2 
+Input:
+a = 2
+b = 3
+c = a**b
+print ("Line 6 - Value of c is ", c)
+a = 10
+b = 5
+c = a//b
+print ("Line 7 - Value of c is ", c)
+
+Output:
+  Line 6 - Value of c is  8
+  Line 7 - Value of c is  2
+  
+# 3
+Input:
+
