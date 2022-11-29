f67943afc5e5d1faaa76108e16c9b9ac791e7dd7
Khafiz <116889143+Khafiz246810@users.noreply.github.com>
2022-11-29 20:45:03 +0600

    Create LOGICAL OPERATORS.py

---
+++ LOGICAL OPERATORS.py
@@ -0,0 +1,18 @@
+Input:
+
+a = 10
+b = 10
+c = -10
+
+if a > 0 and b > 0:
+	print("The numbers are greater than 0")
+
+if a > 0 and b > 0 and c > 0:
+	print("The numbers are greater than 0")
+else:
+	print("Atleast one number is not greater than 0")
+  
+Output:
+  
+The numbers are greater than 0
+Atleast one number is not greater than 0
